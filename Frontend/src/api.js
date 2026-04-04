ac52298fc20e21a63aeadd08fb2a9bdcb492f9da
manan480 <manan480p@gmail.com>
2026-04-04 22:42:45 +0530

    connected frontend to live backend

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // Our FastAPI backend URL
+  baseURL: 'https://hello1-p3ui.onrender.com', // Our FastAPI backend URL
   headers: {
     'Content-Type': 'application/json',
   },
